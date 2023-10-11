a7f03d4023ecbde85959eb322f1bdfbffa9d2f8c
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-10-11 11:57:02 -0600

    updated

--- Assets/Scripts/DiceEvaluator.cs
+++ Assets/Scripts/DiceEvaluator.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class DiceEvaluator : MonoBehaviour
 {
     public static DiceEvaluator Instance;
+
+    public int[] dVH;
 
     public bool lgStr, smStr, fHouse, fourK, threeK, twoP;
 
@@ -13,6 +17,7 @@ public class DiceEvaluator : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            dVH = new int[6];
         }
         else
         {
@@ -24,5 +29,152 @@ public class DiceEvaluator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void DiceValueOnHand(int[] DiceValueCount)
+    {
+        dVH = DiceValueCount;
+    }
+
+    private bool LargeStraightCheck()
+    {
+        bool lrgstr = false;
+
+        int currentCount = 0;
+        int maxValue = 0;
+        
+        for (int i = 0; i < dVH.Length; i++)
+        {
+            if (dVH[i] == 0)
+            {
+                currentCount = 0;
+            }
+            else
+            {
+                currentCount++;
+            }
+
+            if (currentCount > maxValue)
+            {
+                maxValue = currentCount;
+            }
+        }
+
+        if (maxValue == 5)
+        {
+            lrgstr = true;
+        }
+
+        return lrgstr;
+    }
+
+    private bool SmallStraightCheck()
+    {
+        bool smstr = false;
+
+        int currentCount = 0;
+        int maxValue = 0;
+
+        for (int i = 0; i < dVH.Length; i++)
+        {
+            if (dVH[i] == 0)
+            {
+                currentCount = 0;
+            }
+            else
+            {
+                currentCount++;
+            }
+
+            if (currentCount > maxValue)
+            {
+                maxValue = currentCount;
+            }
+        }
+
+        if (maxValue == 4)
+        {
+            smstr = true;
+        }
+
+        return smstr;
+    }
+    private bool FullHouseCheck()
+    {
+        bool three = false, pair = false;
+
+        for (int i = 0; i < dVH.Length; i++)
+        {
+            if (dVH[i] == 3)
+            {
+                three = true;
+            }
+            else
+            {
+                three = false;
+            }
+            if (dVH[i] == 2)
+            {
+                pair = true;
+            }
+            else
+            {
+                pair = false;
+            }
+        }
+
+        return three && pair;
+    }
+    private bool FourKind()
+    {
+        bool fourk = false;
+
+        for (int i = 0; i < dVH.Length; i++ )
+        {
+            if (dVH[i] == 4)
+            {
+                fourk = true;
+                break;
+            }
+        }
+
+        return fourk;
+    }
+    private bool ThreeKind()
+    {
+        bool threek = false;
+
+        for (int i = 0; i < dVH.Length; i++)
+        {
+            if (dVH[i] == 3)
+            {
+                threek = true;
+                break;
+            }
+        }
+
+        return threek;
+    }
+    private bool TwoPair()
+    {
+        bool firstP = false, secondP = false;
+
+        for(int i = 0; i < dVH.Length; i++)
+        {
+            if (dVH[i] == 2)
+            {
+                if (firstP)
+                {
+                    secondP = true;
+                    break;
+                }
+                else
+                {
+                    firstP = true;
+                }
+            }
+        }
+
+        return firstP && secondP;
     }
 }
