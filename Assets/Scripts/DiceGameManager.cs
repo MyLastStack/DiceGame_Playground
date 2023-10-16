9d85120d607224cc7a6a5c9f137ce6b2e31c2b6f
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-10-16 15:32:11 -0600

    Update DiceGameManager.cs

--- Assets/Scripts/DiceGameManager.cs
+++ Assets/Scripts/DiceGameManager.cs
@@ -304,7 +304,6 @@ public class DiceGameManager : MonoBehaviour
                             break;
                         }
                     }
-
                     iStartUp--;
                 }
 
